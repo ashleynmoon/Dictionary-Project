376aa8fd32ad18525d488622aa0877208af3c929
Ashley Moon <79172865+ashleynmoon@users.noreply.github.com>
2021-08-06 12:01:24 -0400

    Formatted dictionary component
    Started to build the search engine for the dictionary app.

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Dictionary(){
+    const [keyword, setKeyword] = useState("");
+
+    function search(event){
+        event.preventDefault();
+        alert(`Searching for ${keyword} definition`);
+    }
+
+    function handleKeywordChange(event){
+        setKeyword(event.target.value);
+    }
+
     return(
         <div className="Dictionary">
-            <form>
+            <form onSubmit={search}>
                 <div>
                   <input
                     type="search"
                     placeholder="Type a word..."
-                    autoFocus="on"                   
-                  />
+                    autoFocus={true}
+                    onChange={handleKeywordChange} />
                 </div>
               </form>
         </div>
