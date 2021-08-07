ae2d5e05581471b052799704e55733e023d6189a
Ashley Moon <79172865+ashleynmoon@users.noreply.github.com>
2021-08-07 10:07:12 -0400

    Added dictionary api intergration
    https://dictionaryapi.dev/

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,19 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 export default function Dictionary(){
     const [keyword, setKeyword] = useState("");
 
+    function handleResponse(response){
+        console.log(response);
+    }
+
     function search(event){
         event.preventDefault();
         alert(`Searching for ${keyword} definition`);
+
+        let apiUrl =`https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+        axios.get(apiUrl).then(handleResponse);
     }
 
     function handleKeywordChange(event){
