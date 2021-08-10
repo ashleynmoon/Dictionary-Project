a628918647f586b57d2a38aff071c50de4463ec9
Ashley Moon <79172865+ashleynmoon@users.noreply.github.com>
2021-08-10 13:52:43 -0400

    Added background image

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 export default function App() {
   return (
-    <div className="App">
+    <div className="App" >
       <div className="container">
         <header className="App-header">
           The Dictionary
